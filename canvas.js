045c6e0677fbc895a7c80fb1a8894be2f2b4646a
Bartosz <bartosz@example.com>
2023-11-12 16:35:15 +0100

    Fix color painting, add second way

--- canvas.js
+++ canvas.js
@@ -8,6 +8,7 @@ const ctx = canvas.getContext('2d')
 
 const pixPerBlockX = canvas.width / fieldColumns
 const pixPerBlockY = canvas.height / fieldRows
+const lineWidthFactor = 0.1
 
 function clearView() {
     ctx.reset()
@@ -21,16 +22,39 @@ function refreshView() {
         for (let ci = 0; ci < fieldColumns; ci++) {
             const color = getFieldColor(ri, ci)
             if (color)
-                paintBlock(ri, ci, color)
+                paintBlock1(ri, ci, color)
         }
     }
 }
 
-// TODO: fix color painting
-function paintBlock(row, col, color) {
+function paintBlock1(row, col, color) {
+    ctx.beginPath()
     ctx.fillStyle = color
+    ctx.lineWidth = 2
     ctx.strokeStyle = 'black';
     ctx.rect(col * pixPerBlockX, row * pixPerBlockY, pixPerBlockX, pixPerBlockY)
     ctx.fill()
     ctx.stroke()
+}
+
+function paintBlock2(row, col, color) {
+    ctx.beginPath()
+    ctx.fillStyle = 'black'
+    ctx.rect(
+        col * pixPerBlockX, 
+        row * pixPerBlockY, 
+        pixPerBlockX, 
+        pixPerBlockY)
+    ctx.fill()
+
+    const shiftX = pixPerBlockX * lineWidthFactor
+    const shiftY = pixPerBlockY * lineWidthFactor
+    ctx.beginPath()
+    ctx.fillStyle = color
+    ctx.rect(
+        col * pixPerBlockX + shiftX, 
+        row * pixPerBlockY + shiftY, 
+        pixPerBlockX - 2 * shiftX, 
+        pixPerBlockY - 2 * shiftY)
+    ctx.fill()
 }
